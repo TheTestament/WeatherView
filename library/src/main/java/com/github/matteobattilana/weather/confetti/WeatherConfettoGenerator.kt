3915855077b0e4b6eecf0341e01b0df25d344e2a
aboutgaurav <gaurav.kumar24@outlook.com>
2018-07-12 11:39:59 +0530

    Change RAIN_STRETCH value to 30.5

--- library/src/main/java/com/github/matteobattilana/weather/confetti/WeatherConfettoGenerator.kt
+++ library/src/main/java/com/github/matteobattilana/weather/confetti/WeatherConfettoGenerator.kt
@@ -17,7 +17,7 @@ class MotionBlurBitmapConfetto(val confettoInfo: ConfettoInfo) : Confetto() {
 
     companion object {
         const val SNOW_RADIUS = 7.5f
-        const val RAIN_STRETCH = 20.5f
+        const val RAIN_STRETCH = 30.5f
     }
 
     private var prevX: Float? = null
